f01df28ae08f761645a858cec442fe955d46f4e2
Augusto1404 <augustopelis4@gmail.com>
2024-11-11 13:46:03 -0500

    add cors

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,9 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+  app.enableCors({
+    origin: '*',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
